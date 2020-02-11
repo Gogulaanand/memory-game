e937b1d78628a01085d521a9a5b30d5f01570100
Gogulaanand R <gogulaanand02@gmail.com>
2020-02-11 23:49:44 +0530

    added dynamic shuffled cards, user choice of number of sets

--- mem_game.js
+++ mem_game.js
@@ -2,26 +2,48 @@ var facingUpCount = 0;
 var cardsFacingUp = [];
 var score=0;
 var board = document.getElementById('board');
+var images = ['pair1', 'pair2', 'pair3', 'pair4', 'pair5', 'pair6', 'pair7', 'pair8', 'pair9', 'pair10', 'pair11', 'pair12'];
+var desiredCount = 0;
+var shuffledPairs = [];
 
 if(document.readyState !== "loading"){
-  setup();
+  getDesiredCount('pairsCount');
 }
 else{
   document.addEventListener('DOMContentLoaded', function(event){
     event.preventDefault();
-    setup();
+    getDesiredCount('pairsCount');
   });
 }
 
-function setup(){
-  // var collection = new DocumentFragment();
-  // for(var i=0; i<12; i++){
-  //   card = document.createElement('div');
-  // var arr = ['card', `card${i+1}`]
-  //   card.classList.add(...arr);
-  //   collection.appendChild(card);
-  // }
-  // board.appendChild(collection);
+function getDesiredCount(dropDownId){
+	var el = document.getElementById(dropDownId);
+	desiredCount = parseInt(el.options[el.selectedIndex].value);
+	shuffledPairs = shuffle(images.slice(0,desiredCount));
+	console.log(shuffledPairs);
+	setup(shuffledPairs);
+}
+
+function setup(shuffledPairs){
+	document.getElementById('board').innerHTML = '';
+  var collection = new DocumentFragment();
+	var len =  shuffledPairs.length;
+  for(var i=0; i < len; i++){
+    card = document.createElement('div');
+  var arr = ['card', `card${i+1}`, shuffledPairs[i]]
+    card.classList.add(...arr);
+    collection.appendChild(card);
+  }
+	if(len == 12){
+		board.style.cssText = "width: calc(120px*4 + 20px*3 ); height: calc(120px*3 + 20px*3); display: grid; grid-gap: 20px; grid-template-columns: repeat(4, 1fr); grid-template-rows: repeat(3, 1fr);";
+	}
+	else if(len==16){
+		board.style.cssText = "width: calc(120px*4 + 20px*3 ); height: calc(120px*4 + 20px*4); display: grid; grid-gap: 20px; grid-template-columns: repeat(4, 1fr); grid-template-rows: repeat(4, 1fr);";
+	}
+	else if(len==24){
+		board.style.cssText = "width: calc(120px*6 + 20px*5 ); height: calc(120px*4 + 20px*4); display: grid; grid-gap: 20px; grid-template-columns: repeat(6, 1fr); grid-template-rows: repeat(3, 1fr);";
+	}
+  board.appendChild(collection);
   var listOfCards = document.getElementsByClassName('card');
   for(let card of listOfCards){
     card.addEventListener('click', flip);
@@ -31,10 +53,11 @@ function setup(){
 function flip(){
 	if(facingUpCount<2){
 		cardsFacingUp.push(this);
+		this.removeEventListener('click', flip);
 		this.style.border = '1px solid #ebf5fc';
 		facingUpCount+=1;
 		if(facingUpCount==2){
-			setTimeout(checkForMatch(cardsFacingUp), 0);
+			checkForMatch(cardsFacingUp);
 		}
 	}
 };
@@ -55,9 +78,34 @@ function checkForMatch(list){
 	}
 	else{
 		console.log('no match');
+		card1.addEventListener('click', flip);
+		card2.addEventListener('click', flip);
 		card1.style.border = '60px solid #ebf5fc';
 		card2.style.border = '60px solid #ebf5fc';
 	}
 	facingUpCount = 0;
 	cardsFacingUp.length = 0;
+}
+
+function shuffle(array) {
+	array = array.reduce(function(res,current){
+		return res.concat([current, current]);
+	}, []);
+
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
